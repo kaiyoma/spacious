cfaca63c8402d05f9fa1d9d70910f595f9226802
kaiyoma <kyle.getz@gmail.com>
2020-02-17 16:52:04 -0800

    Change serif font to Lora

--- header.php
+++ header.php
@@ -50,6 +50,8 @@
 
 <!-- Add third-party imports -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet">
+
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
